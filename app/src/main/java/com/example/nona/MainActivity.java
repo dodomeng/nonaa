40cd4a32f136bd0963163f16e71f83b3c966d9aa
DO <ehrbs2992@naver.com>
2022-11-21 23:34:09 +0900

    ㅎㅎ

---
+++ app/src/main/java/com/example/nona/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.nona;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView list;
+    String mTitle[]={"브리또","상호명","상호명","상호명","상호명","상호명","상호명","상호명","상호명"};//listview에 title부분 설정
+    String mDescription[]={"2시에 브리또 드실 분","글 내용","글 내용","글 내용","글 내용","글 내용","글 내용","글 내용","글 내용"};//listview에 설명부분
+    int images[]={R.drawable.buri,R.drawable.buri,R.drawable.buri,R.drawable.buri,R.drawable.buri,R.drawable.buri,R.drawable.buri,R.drawable.buri,R.drawable.buri};
+    //listview에 들어가는 사진
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list=(ListView)findViewById(R.id.list);
+
+        MyAdapter adapter=new MyAdapter(this,mTitle,mDescription,images);
+        list.setAdapter(adapter);//리스트에 어뎁터 설정
+
+    }
+    class MyAdapter extends ArrayAdapter<String>{
+
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter(Context c, String title[],String description[],int imgs[]){
+            super(c,R.layout.board,R.id.textView1,title);
+            this.context=c;
+            this.rTitle=title;
+            this.rDescription=description;
+            this.rImgs=imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            //앞에서 만든 row xml파일을 view 객체로 만들기 위해서는 layoutInflater를 이용
+            LayoutInflater layoutInflater=(LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View board=layoutInflater.inflate(R.layout.board,parent,false);
+
+            ImageView images=board.findViewById(R.id.image);
+            TextView myTitle=board.findViewById(R.id.textView1);
+            TextView myDescription=board.findViewById(R.id.textView2);
+
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+
+            return board;//앞에서 만든 xml 파일..
+        }
+    }
+
+}
